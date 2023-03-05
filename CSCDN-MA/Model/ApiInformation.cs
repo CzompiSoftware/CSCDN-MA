d0ecc149fa6188585b91c3b4387185f7c9757cba
Dávid Czompó <czompi@czompi.hu>
2023-03-06 00:42:52 +0100

    Update ApiInformation.cs

--- CSCDN-MA/Model/ApiInformation.cs
+++ CSCDN-MA/Model/ApiInformation.cs
@@ -1,33 +1,31 @@
-﻿using System;
+﻿namespace CSCDNMA.Model;
 
-namespace CSCDNMA.Model
+internal class ApiInformation
 {
-    internal class ApiInformation
-    {
-        public ApiInformation(DateTime startTime)
-        {
+#nullable enable
+	public ApiInformation(DateTime startTime, string? id = null)
+#nullable disable
+	{
 #if STAGING
-            Type = "STAGING";
+        Type = "STAGING";
 #elif BETA
-            Type = "BETA";
+		Type = "BETA";
 #elif RELEASE
-            Type = "RELEASE";
+        Type = "RELEASE";
 #else
-            Type = "INVLAID";
+        Type = "INVLAID";
 #endif
-            Build = Builtin.BuildId;
-            StartTime = DateTime.Parse(startTime.ToString("yyyy'.'MM'.'dd'T'HH':'mm':'ss"));
-            Id = Guid.NewGuid(); // Use app param as guid or create a new one when not set. 
-            CompileTime = Builtin.CompileTime;
-            //var ver = CzomPack.Settings.Application.Assembly.GetName().Version;
-            //Version = $"{ver.ToString(3)}-build{ver.Revision:00}";
-        }
-
-        public Guid Id { get; internal set; }
-        public string Type { get; internal set; }
-        public string Version { get; internal set; }
-        public DateTime CompileTime { get; internal set; }
-        public DateTime StartTime { get; internal set; }
-        public Guid Build { get; internal set; }
-    }
+		Build = Builtin.BuildId;
+		StartTime = DateTime.Parse(startTime.ToString("yyyy'.'MM'.'dd'T'HH':'mm':'ss"));
+		Id = id is not null ? Guid.Parse(id) : Guid.NewGuid(); // Use app param as guid or create a new one when not set. 
+		CompileTime = Builtin.CompileTime;
+		var ver = CzomPack.Settings.Application.Assembly.GetName().Version;
+		Version = $"{ver.ToString(3)}-build{ver.Revision:00}";
+	}
+	public Guid Id { get; internal set; }
+	public string Type { get; internal set; }
+	public string Version { get; internal set; }
+	public DateTime CompileTime { get; internal set; }
+	public DateTime StartTime { get; internal set; }
+	public Guid Build { get; internal set; }
 }
